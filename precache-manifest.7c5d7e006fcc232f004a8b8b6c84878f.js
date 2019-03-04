f90e8ec42a6acc40accee9a03a28a6cfe1b2e521
Piotr <piotr.nicinski93@gmail.com>
2019-03-04 22:08:02 +0100

    Updates

---
+++ precache-manifest.7c5d7e006fcc232f004a8b8b6c84878f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5ed97bafe1e870f409",
+    "url": "/react-robots/static/js/runtime~main.5d5ed97b.js"
+  },
+  {
+    "revision": "1e53e4d60d7079879ddd",
+    "url": "/react-robots/static/js/main.1e53e4d6.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/react-robots/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "1e53e4d60d7079879ddd",
+    "url": "/react-robots/static/css/main.9082f1a7.chunk.css"
+  },
+  {
+    "revision": "092bbd9f12b181f7f8b9ad249c17d4c0",
+    "url": "/react-robots/index.html"
+  }
+];
